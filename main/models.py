17c79f01d5597ef71f852ac0e928f334e040285e
ajh123 <ajh123@users.noreply.github.com>
2022-12-15 17:50:44 +0000

    view shoping lists

--- main/models.py
+++ main/models.py
@@ -6,7 +6,7 @@ class Item(models.Model):
     price = models.FloatField(default=0)
 
     def __str__(self):
-        return f"{self.numberInStock} x {self.name}"
+        return f"{self.name} (£{self.price} each)"
     
 
 class ShopingListItem(models.Model):
@@ -16,8 +16,23 @@ class ShopingListItem(models.Model):
     def __str__(self):
         return f"{self.numberWanted} x {self.item.name}"
 
+    def getTotalPrice(self):
+        return self.item.price * self.numberWanted
+
 
 class ShopingList(models.Model):
     items = models.ManyToManyField(ShopingListItem)
     totalPrice = models.FloatField(default=0)
     pubDate = models.DateTimeField('date published')
+
+    def getTotalPrice(self):
+        t = 0
+        for item in self.items.all():
+            t += item.getTotalPrice()
+        return t
+
+    def getTotalCount(self):
+        t = 0
+        for item in self.items.all():
+            t += item.numberWanted
+        return t
--- main/views.py
+++ main/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from .models import ShopingList
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the home index.")
+    latest_list = ShopingList.objects.order_by('-pubDate')[:5]
+    context = {
+        'latest_list': latest_list,
+    }
+    return render(request, 'main/index.html', context)
+
+def shopingList(request, listID):
+    list = get_object_or_404(ShopingList, pk=listID)
+    return render(request, 'main/viewList.html', {
+        'list': list, 
+        'listID':listID
+    })
